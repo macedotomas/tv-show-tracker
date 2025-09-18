7b908ab673c072093146be4bccf8ad9b8e13ced0
Tomás Macedo <up202008551@edu.fe.up.pt>
2025-09-19 00:20:15 +0100

    delete tv show

--- frontend/src/components/TvShowCard.jsx
+++ frontend/src/components/TvShowCard.jsx
@@ -1,8 +1,12 @@
 import { EditIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useTvShowStore } from '../stores/useTvShowStore.jsx'
 
 function TvShowCard({ tvShow }) {
+
+  const { deleteTvShow } = useTvShowStore();
+
   return (
     <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
       {/* TvShow Details */}
@@ -19,7 +23,7 @@ function TvShowCard({ tvShow }) {
           <Link to={`/tv-shows/${tvShow.show_id}`} className="btn btn-sm btn-info btn-outline">
             <EditIcon className="size-4" />
           </Link>
-          <button className="btn btn-sm btn-error btn-outline">
+          <button className="btn btn-sm btn-error btn-outline" onClick={() => deleteTvShow(tvShow.show_id)}>
             <Trash2Icon className="size-4" />
           </button>
 
--- frontend/src/stores/useTvShowStore.jsx
+++ frontend/src/stores/useTvShowStore.jsx
@@ -1,5 +1,6 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 
 const BASE_URL = 'http://localhost:3000';
@@ -7,25 +8,44 @@ const BASE_URL = 'http://localhost:3000';
 
 export const useTvShowStore = create((set, get) => ({
   tvShows: [],
-  loading:false,
-  error:null,
+  loading: false,
+  error: null,
 
   fetchTvShows: async () => {
-    set({loading:true, error:null});
+    set({ loading: true, error: null });
     try {
       const response = await axios.get(`${BASE_URL}/api/tv-shows`);
-      
+
       // first data from response is from axios, second data is from backend response 
-      set({tvShows: response.data.data, error:null});
+      set({ tvShows: response.data.data, error: null });
 
     } catch (error) {
-      
-      set({error: error.message, loading:false});
-    
+
+      set({ error: error.message, loading: false });
+
     } finally {
-      set({loading:false});
+      set({ loading: false });
     }
 
+  },
+
+  deleteTvShow: async (id) => {
+    try {
+      set({ loading: true });
+      const token = localStorage.getItem("token") || '';
+      await axios.delete(`${BASE_URL}/api/tv-shows/${id}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      // Update the local state to remove the deleted TV show
+      set({ tvShows: get().tvShows.filter(tvShow => tvShow.show_id !== id) });
+      toast.success("TV show deleted successfully.");
+    } catch (error) {
+      set({ error: error.message });
+      toast.error("Failed to delete TV show.");
+    } finally {
+      set({ loading: false });
+    }
   }
+
 }));
 
